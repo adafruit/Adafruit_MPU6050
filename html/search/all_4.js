ae03f8aa3d1ff4723db91df0c9f15f243b807417
Doxygen CI <travis@travis-ci.org>
2020-03-06 00:51:22 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['enablecycle',['enableCycle',['../class_adafruit___m_p_u6050.html#ad399250052871e4c5eff319c7a9f07ff',1,'Adafruit_MPU6050']]],
-  ['enablesleep',['enableSleep',['../class_adafruit___m_p_u6050.html#a918747c07e8ee2df3d3db0c01e93246c',1,'Adafruit_MPU6050']]]
+  ['enablecycle',['enableCycle',['../class_adafruit___m_p_u6050.html#a096769a306626bdfb665ddc13dffdb4b',1,'Adafruit_MPU6050']]],
+  ['enablesleep',['enableSleep',['../class_adafruit___m_p_u6050.html#a2a819036ffbf2b84fdf10853f266c980',1,'Adafruit_MPU6050']]]
 ];
