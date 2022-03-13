683939cb4d49c27087ccf3168f31c0ee1622fa9a
Doxygen CI <ci-arduino@invalid>
2022-03-13 23:21:06 +0000

    Deploy docs to GitHub Pages from commit dd977d804d
    Commit: dd977d804dbb0286c0fe632e9945575d3b3460b6
    GitHub Actions run: 1977798297

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -1,11 +1,13 @@
 var searchData=
 [
   ['setaccelerometerrange',['setAccelerometerRange',['../class_adafruit___m_p_u6050.html#a1583d1351bb907d3823aee36af0efe5f',1,'Adafruit_MPU6050']]],
+  ['setaccelerometerstandby',['setAccelerometerStandby',['../class_adafruit___m_p_u6050.html#aba5376fae68d23e34ec5bc583ac72a5b',1,'Adafruit_MPU6050']]],
   ['setclock',['setClock',['../class_adafruit___m_p_u6050.html#a3af6a973a7b12175b6bf7de62944c9c5',1,'Adafruit_MPU6050']]],
   ['setcyclerate',['setCycleRate',['../class_adafruit___m_p_u6050.html#a2010ff1e161a8fa54dfed0fd9a7a796b',1,'Adafruit_MPU6050']]],
   ['setfilterbandwidth',['setFilterBandwidth',['../class_adafruit___m_p_u6050.html#ab818534e87053b58966934d12fe57051',1,'Adafruit_MPU6050']]],
   ['setfsyncsampleoutput',['setFsyncSampleOutput',['../class_adafruit___m_p_u6050.html#a5f0eb5af35196e6e7bb910f080ac8152',1,'Adafruit_MPU6050']]],
   ['setgyrorange',['setGyroRange',['../class_adafruit___m_p_u6050.html#a0a2a0b17f69e93b2ad0d8c2949178b88',1,'Adafruit_MPU6050']]],
+  ['setgyrostandby',['setGyroStandby',['../class_adafruit___m_p_u6050.html#ab98f5dde9b0385ffc728f41cf5212ac9',1,'Adafruit_MPU6050']]],
   ['sethighpassfilter',['setHighPassFilter',['../class_adafruit___m_p_u6050.html#a8d561f79d7a66a2cc1056620f6a3ebb4',1,'Adafruit_MPU6050']]],
   ['seti2cbypass',['setI2CBypass',['../class_adafruit___m_p_u6050.html#af7a9b27f8cb02872e91a6eaaae22ba52',1,'Adafruit_MPU6050']]],
   ['setinterruptpinlatch',['setInterruptPinLatch',['../class_adafruit___m_p_u6050.html#a5d4cc0fcf067467e47eaaa42fe3f59ec',1,'Adafruit_MPU6050']]],
@@ -13,5 +15,6 @@ var searchData=
   ['setmotiondetectionduration',['setMotionDetectionDuration',['../class_adafruit___m_p_u6050.html#ad74886bec9f18e251853e681bd3e34a2',1,'Adafruit_MPU6050']]],
   ['setmotiondetectionthreshold',['setMotionDetectionThreshold',['../class_adafruit___m_p_u6050.html#a023b9436cfc265f11967048d98251e61',1,'Adafruit_MPU6050']]],
   ['setmotioninterrupt',['setMotionInterrupt',['../class_adafruit___m_p_u6050.html#a540b1b6b9ba1f3928cb9a47cf0110f42',1,'Adafruit_MPU6050']]],
-  ['setsampleratedivisor',['setSampleRateDivisor',['../class_adafruit___m_p_u6050.html#afd946ff22ebb7cb55e5dbf080f995c5d',1,'Adafruit_MPU6050']]]
+  ['setsampleratedivisor',['setSampleRateDivisor',['../class_adafruit___m_p_u6050.html#afd946ff22ebb7cb55e5dbf080f995c5d',1,'Adafruit_MPU6050']]],
+  ['settemperaturestandby',['setTemperatureStandby',['../class_adafruit___m_p_u6050.html#adc8464a65e9723d0a153b4929fb80ae0',1,'Adafruit_MPU6050']]]
 ];
