4bb6dfdbcce4ebd080620d66b2636e7b194e2b7f
caternuson <caternuson@gmail.com>
2022-09-21 10:53:20 -0700

    add startup retry delay

--- Adafruit_MPU6050.cpp
+++ Adafruit_MPU6050.cpp
@@ -75,9 +75,17 @@ bool Adafruit_MPU6050::begin(uint8_t i2c_address, TwoWire *wire,
 
   i2c_dev = new Adafruit_I2CDevice(i2c_address, wire);
 
-  if (!i2c_dev->begin()) {
-    return false;
+  // For boards with I2C bus power control, may need to delay to allow
+  // MPU6050 to come up after initial power.
+  bool mpu_found = false;
+  for (uint8_t tries = 0; tries < 5; tries++) {
+    mpu_found = i2c_dev->begin();
+    if (mpu_found)
+      break;
+    delay(10);
   }
+  if (!mpu_found)
+    return false;
 
   Adafruit_BusIO_Register chip_id =
       Adafruit_BusIO_Register(i2c_dev, MPU6050_WHO_AM_I, 1);
